fb3c716594995a6541a92ea07e5401cb2ddb226b
sajjadsuri8-source <sajjadsuri8@gmail.com>
2026-09-22 02:31:07 +0330

    Add application entry point

---
+++ Panel/Program.cs
@@ -0,0 +1,12 @@
+namespace PanelApp;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+        Application.Run(new MainForm());
+    }
+}
